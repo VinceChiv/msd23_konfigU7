eba51fc2bada2e4becac900e7f35f70ee2ae9db6
Matio Tawdrous <matio.tawdrous@fh-joannneum.at>
2024-07-02 14:16:28 +0200

    made Cocktail similiar to SimpleDrink

--- src/main/java/at/fhj/msd/Cocktail.java
+++ src/main/java/at/fhj/msd/Cocktail.java
@@ -3,35 +3,77 @@ package at.fhj.msd;
 import java.util.LinkedList;
 
 /**
- * GITTeamWork
- * <Description>
- * Author: Matio
- * Last Change: 02.07.2024
+ * Represents a cocktail which is a drink with multiple ingredients
  */
 public class Cocktail extends Drink {
-    private String name;
-    private LinkedList<Liquid> ingredients;
+    /**
+     * list of liquids for the cocktail
+     */
+    protected LinkedList<Liquid> ingredients;
 
+    /**
+     * Creates a Cocktail object with given name
+     *
+     * @param name name of the cocktail
+     */
     public Cocktail(String name) {
         super(name);
-        this.name = name;
-        this.ingredients = new LinkedList<Liquid>();
+        ingredients = new LinkedList<Liquid>();
     }
 
+    /**
+     * Adds a liquid to the cocktail
+     *
+     * @param l the liquid to add
+     */
+    public void addIngredient(Liquid l) {
+        ingredients.add(l);
+    }
+
+    /**
+     * Returns the number of ingredients
+     *
+     * @return number of ingredients
+     */
+    public int getNumberOfIngredients() {
+        return ingredients.size();
+    }
+
+    /**
+     * Returns the volume of the cocktail
+     *
+     * @return the volume of the cocktail in liter
+     */
     @Override
     public double getVolume() {
-        return 0;
+        double volume = 0;
+        for (Liquid l : ingredients) {
+            volume += l.getVolume();
+        }
+        return volume;
     }
 
+    /**
+     * Returns the alcohol volume percent of the cocktail
+     *
+     * @return alcohol volume percent
+     */
     @Override
     public double getAlcoholPercent() {
-        return 0;
+        double alcohol = 0;
+        for (Liquid l : ingredients) {
+            alcohol += l.getAlcoholPercent() * l.getVolume();
+        }
+        return alcohol / getVolume();
     }
 
+    /**
+     * Gives information if cocktail is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
     @Override
     public boolean isAlcoholic() {
-        return false;
+        return getAlcoholPercent() > 0;
     }
-
-
 }
