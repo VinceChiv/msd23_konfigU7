b5905a069ebf17008aa7c7c039f03bb2dd72a6a9
Matio Tawdrous <matio.tawdrous@fh-joannneum.at>
2024-07-02 12:18:21 +0200

    real maven project structure

--- src/main/java/at/fhj/msd/IQueue.java
+++ src/main/java/at/fhj/msd/IQueue.java
@@ -13,23 +13,23 @@ public interface IQueue {
    * 
    * i fell asleep on my keyboard...
    * 
-   * */ 
-  public abstract boolean offer(String obj);
+   * */
+  boolean offer(String obj);
 
   //returns + del 1st element; null if nothing in there
    //
-  public abstract String poll();
+  String poll();
 
   /* 
   same as poll, if there is nothing in there error NoSuchElementException
    */
-  public abstract String remove();
+  String remove();
 
   // 1st element without dlete, otherwise null
    
-  public abstract String peek();
+  String peek();
 
   // element is to peek what remove is to poll
   
-  public abstract String element();
+  String element();
 }
